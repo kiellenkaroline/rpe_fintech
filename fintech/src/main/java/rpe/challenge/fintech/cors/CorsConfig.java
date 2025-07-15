df605e1f89fd96bcda9019267706088c8265acd4
Kiellen <kiellenkarolinne123@icloud.com>
2025-07-15 00:56:43 -0300

    Atualiza cors

--- fintech/src/main/java/rpe/challenge/fintech/cors/CorsConfig.java
+++ fintech/src/main/java/rpe/challenge/fintech/cors/CorsConfig.java
@@ -1,26 +1,18 @@
 package rpe.challenge.fintech.cors;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
 }
 
-
+}
